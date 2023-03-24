0d3a8fef18493ca7a2228c89a9a9474ead2bb6e1
Taisen Colcher <TaisenColcher@Gmail.com>
2023-03-24 16:38:02 -0400

    Add DevTarget Class

---
+++ Source/Spring2022_Capstone/Weapon/DevTargets.cpp
@@ -0,0 +1,35 @@
+// Created by Spring2022_Capstone team
+
+
+#include "DevTargets.h"
+
+// Sets default values
+ADevTargets::ADevTargets()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TargetStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("Static Mesh Comp");
+	//TargetSkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>("Skeletal Mesh Component");
+
+	RootComponent = TargetStaticMesh;
+	
+}
+
+// Called when the game starts or when spawned
+void ADevTargets::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ADevTargets::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ADevTargets::ToggleMaterial()
+{
+	TargetStaticMesh->SetMaterial(0, HitMaterial);
+}
+
