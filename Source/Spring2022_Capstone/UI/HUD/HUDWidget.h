449d732ff34b9f38be30a7e84dbf449c06310bcb
Alejandro Ulloa <alejandroekas@gmail.com>
2023-03-23 21:50:13 -0400

    HUD base classes created

---
+++ Source/Spring2022_Capstone/UI/HUD/HUDWidget.h
@@ -0,0 +1,17 @@
+// Created by Spring2022_Capstone team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HUDWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPRING2022_CAPSTONE_API UHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
