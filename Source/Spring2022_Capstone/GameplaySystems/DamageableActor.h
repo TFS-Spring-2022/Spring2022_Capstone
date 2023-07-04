05c366a7c18eb5d1196f3570d6c912c7fcf0c6dc
Taisen Colcher <TaisenColcher@Gmail.com>
2023-07-04 19:48:59 -0400

    Create DamageableActor UInterface

---
+++ Source/Spring2022_Capstone/GameplaySystems/DamageableActor.h
@@ -0,0 +1,25 @@
+// Created by Spring2022_Capstone team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "DamageableActor.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UDamageableActor : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class SPRING2022_CAPSTONE_API IDamageableActor
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
