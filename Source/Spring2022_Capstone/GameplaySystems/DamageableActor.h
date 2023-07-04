975cce5b23cababd761df0bf9a47898a1fba7ace
Taisen Colcher <TaisenColcher@Gmail.com>
2023-07-04 19:53:29 -0400

    Make DamageableActor Interface BlueprintType

--- Source/Spring2022_Capstone/GameplaySystems/DamageableActor.h
+++ Source/Spring2022_Capstone/GameplaySystems/DamageableActor.h
@@ -7,7 +7,7 @@
 #include "DamageableActor.generated.h"
 
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, BlueprintType)
 class UDamageableActor : public UInterface
 {
 	GENERATED_BODY()
