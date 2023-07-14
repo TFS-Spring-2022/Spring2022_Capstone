b7170cd16c82896c0f8a5a5a03eec7f1e9e834aa
Taisen Colcher <TaisenColcher@Gmail.com>
2023-07-14 01:14:07 -0400

    ReDo RecoilComponent Functionality
    Apologies for single big commit. Weapon recoil and recovery work and take player mouse input into account. BeginPlay() now checks weapon's fire rate and uses different properties if they are above 0.5f.

--- Source/Spring2022_Capstone/Weapon/RecoilComponent.cpp
+++ Source/Spring2022_Capstone/Weapon/RecoilComponent.cpp
@@ -3,28 +3,114 @@
 
 #include "RecoilComponent.h"
 
-// Sets default values for this component's properties
+#include "GameFramework/PawnMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
 URecoilComponent::URecoilComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 }
 
-// Called when the game starts
 void URecoilComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
 	OwningParentWeapon = Cast<AWeaponBase>(GetOwner()); 
 	OwnersPlayerController = GetWorld()->GetFirstPlayerController();
+	OwnersPawnMovementComponent = OwnersPlayerController->GetPawn()->GetMovementComponent();
+
+	// Check if weapon has a 'larger' fire rate
+	bHasLargerFireRate = (OwningParentWeapon->FireRate >= LargeFireRateSize) ? true : false;
+	
+	RecoilReset();
 	
 }
 
-// Called every frame
+// Called when weapon is fired
+void URecoilComponent::RecoilKick()
+{
+
+	// If first shot in batch, save Player's Control Rotation.
+	if(bIsRecoiling == false)
+		RecoilStartRotation = UKismetMathLibrary::NormalizedDeltaRotator(OwnersPlayerController->GetControlRotation(), FRotator::ZeroRotator);
+	
+	// Start timer to ensure RecoilStartRotation is not overwritten when firing multiple times.
+	if(bHasLargerFireRate)
+		// Weapon's with larger fire rates cannot use their FireRate to go into recovery, they must use LargeFireRateRecoveryStartTime.
+		GetWorld()->GetTimerManager().SetTimer(FireTimerHandle, this, &URecoilComponent::FireTimerHandleFunction, LargeFireRateRecoveryStartTime, false);
+	else
+		GetWorld()->GetTimerManager().SetTimer(FireTimerHandle, this, &URecoilComponent::FireTimerHandleFunction, (OwningParentWeapon->FireRate + FireTimeBuffer), false);
+	
+	TimesFired++;
+
+	// Knock up Player's Control Rotation.
+	RecoilRotation = FRotator(OwnersPlayerController->GetControlRotation().Pitch - VerticalKickAmount, OwnersPlayerController->GetControlRotation().Yaw, OwnersPlayerController->GetControlRotation().Roll);
+	OwnersPlayerController->SetControlRotation(RecoilRotation);
+
+	// set bIsRecoiling true to calculate Player's mouse movement correction in TickComponent(). 
+	bIsRecoiling = true;
+	bIsRecovering = false;
+}
+
+void URecoilComponent::FireTimerHandleFunction()
+{
+	// Player has stopped firing, begin recovery.
+	bIsRecoiling = false;
+	bIsRecovering = true;
+}
+
 void URecoilComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	
+	if(bIsRecoiling)
+	{
+		// Account for Player's mouse movement while weapon is recoiling.
+		PitchRecoveryAmount = OwnersPlayerController->GetControlRotation().Pitch + (TimesFired * VerticalKickAmount);
+		RecoveryRotation = FRotator(PitchRecoveryAmount, OwnersPlayerController->GetControlRotation().Yaw, OwnersPlayerController->GetControlRotation().Roll); /** Removing this FRotator and using PitchRecoveryAmount inside CorrectionRotation.Pitch will cause issues **/
+	}
+	if(bIsRecovering)
+	{
+		// Set backup recovery timer to prevent control being taken from player.
+		if(!GetWorld()->GetTimerManager().IsTimerActive(BackupRecoveryTimer))
+			GetWorld()->GetTimerManager().SetTimer(BackupRecoveryTimer, this, &URecoilComponent::RecoilReset,  (bHasLargerFireRate) ? LargeFireRateMaxTimeInRecovery : OwningParentWeapon->FireRate + SemiAutomaticMaxTimeInRecoveryBuffer, false);
+
+		RecoverRecoil(DeltaTime);
+	}
+}
+
+void URecoilComponent::RecoverRecoil(float Time)
+{
+	// If the Player is grounded and their aim has been knocked upwards
+	if(OwnersPawnMovementComponent->IsMovingOnGround() && OwnersPlayerController->GetControlRotation().Pitch > RecoilStartRotation.Pitch)
+	{
+		// Reset the vertical recoil using RecoveryRotation calculated in bIsRecoiling, but maintain horizontal change.
+		CorrectionRotation = UKismetMathLibrary::NormalizedDeltaRotator(FRotator(RecoveryRotation.Pitch, OwnersPlayerController->GetControlRotation().Yaw, OwnersPlayerController->GetControlRotation().Roll), FRotator::ZeroRotator); 
+			
+		// Bring down Player's Control Rotation to CorrectionRotation/RecoveryRotation.
+		OwnersPlayerController->SetControlRotation(UKismetMathLibrary::RInterpTo(OwnersPlayerController->GetControlRotation(), CorrectionRotation, Time, RecoverySpeed));
+
+		// Convert rotations to quaternions to ensure recoil recovers properly.
+		FQuat CurrentRotationAsQuat = UKismetMathLibrary::NormalizedDeltaRotator(OwnersPlayerController->GetControlRotation(), FRotator::ZeroRotator).Quaternion();
+		FQuat RecoveryTargetRotationAsQuat = UKismetMathLibrary::NormalizedDeltaRotator(FRotator(CorrectionRotation.Pitch, OwnersPlayerController->GetControlRotation().Yaw, OwnersPlayerController->GetControlRotation().Roll), FRotator::ZeroRotator).Quaternion();
+
+		// When the Player's Control Rotation has recovered downwards enough, end recovery.	
+		if(CurrentRotationAsQuat.Equals(RecoveryTargetRotationAsQuat, RecoilRecoveryTolerance))
+			RecoilReset();
+	}
+	else
+		RecoilReset();
+}
+
+void URecoilComponent::RecoilReset()
+{
+	bIsRecoiling = false;
+	bIsRecovering = false;
+	TimesFired = 0;
+	
+	RecoilRotation = FRotator::ZeroRotator;
+	RecoveryRotation = FRotator::ZeroRotator;
+	PitchRecoveryAmount = 0;
+
+	GetWorld()->GetTimerManager().PauseTimer(BackupRecoveryTimer);
 }
--- Source/Spring2022_Capstone/Weapon/RecoilComponent.h
+++ Source/Spring2022_Capstone/Weapon/RecoilComponent.h
@@ -26,13 +26,89 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	// Called when a weapon is fired to begin the recoil process.
+	void RecoilKick();
+
 private:
+
+	// Recover the Player's Control Rotation while taking mouse movement into account. 
+	void RecoverRecoil(float Time);
+
+	// Resets all properties used in the recoil process.
+	UFUNCTION()
+	void RecoilReset();
 	
-	/**
-	 * @brief WeaponBase the RecoilComponent is attached to. 
-	 */
+// Components
+	// WeaponBase the RecoilComponent is attached to.
+	UPROPERTY()
 	AWeaponBase* OwningParentWeapon; 
-	
+
+	// Player Controller handling weapon.
+	UPROPERTY()
 	APlayerController* OwnersPlayerController;
+
+	// Movement Component of Player handling weapon.
+	UPROPERTY()
+	UPawnMovementComponent* OwnersPawnMovementComponent;
+
+// Rotators
+	// Player's Control Rotation at the start of recoil.
+	FRotator RecoilStartRotation;
+	
+	// Used to calculate Player Control Rotation's pitch after vertical kick.
+	FRotator RecoilRotation;
+
+	// Used to calculate Player Control Rotation's pitch while taking Player's mouse movement into account. 
+	FRotator RecoveryRotation;
+
+	// Temporary Rotator used to recover Player's Control Rotation.
+	FRotator CorrectionRotation;
+
+// Properties
+	/**
+	 * @brief Used to handle recoil on weapon's with larger fire rates ( > 0.5).
+	 * Sets a specific FireTimerHandle length and RecoilRecoveryTimer so player's do not
+	 * lose control of their aim. Set inside BeginPlay().
+	 */
+	bool bHasLargerFireRate;
+
+	/**
+	 * @brief Amount the Player's Control Rotation will be forced upwards on a shot.
+	 */
+	UPROPERTY(EditAnywhere, meta=(ClampMin = " -89.9", ClampMax = "0"), Category = "Recoil | Properties")
+	float VerticalKickAmount;
+
+	// Speed the Player's Control Rotation is moved during recoil recovery.
+	UPROPERTY(EditAnywhere, Category = "Recoil | Properties")
+	float RecoverySpeed = 10.0f;
+
+// Runtime
+	bool bIsRecoiling;
+	bool bIsRecovering;
+
+	// Number of shots fired in current batch.
+	int TimesFired;
+
+	// The amount Player's Control Rotation needs to be brought down vertically during recoil recovery.
+	float PitchRecoveryAmount;
+
+// Timers
+	// Used to start recoil recovery after player has finished shooting.
+	FTimerHandle FireTimerHandle;
+
+	// Called automatically from RecoilKick(), no need to call directly.
+	UFUNCTION()
+	void FireTimerHandleFunction();
+
+	// Used to cancel recovery if given time has passed to prevent Player losing control.
+	FTimerHandle BackupRecoveryTimer;
+	
+// Const Variables
+	const float LargeFireRateSize = 0.5f;						// Fire rates larger then this size are considered large.
+	const float FireTimeBuffer = 0.20f;							// Buffer used in FireTimer to check if the player is still firing.
+	const float SemiAutomaticMaxTimeInRecoveryBuffer = 0.005f;	// Added to a weapon's fire rate to set the max time(s) a semi automatic can be spent recovering. Used ot ensure player does not lose control.
+	const float LargeFireRateMaxTimeInRecovery = 0.10f;			// The max time(s) a weapon with large fire rate (i.e. Shotgun) can be spent recovering. Used to ensure player does not lose control.
+	const float LargeFireRateRecoveryStartTime = 0.19f;			// Time(s) before recoil recovery starts with a large fire rate weapon.
+	const float RecoilRecoveryTolerance = 0.005f;				// Used when comparing Player's current rotation to recovery rotation point to check if finished.
 	
 };
