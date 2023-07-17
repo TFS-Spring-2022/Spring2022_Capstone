fb6a190f32cdec83e3ac9bec1a17d551dfed9ae8
Taisen Colcher <TaisenColcher@Gmail.com>
2023-07-17 13:16:19 -0400

    Create UpgradeMenuWidget Class
    Created a UserWidget derived UpgradeMenuWidgetClass.

---
+++ Source/Spring2022_Capstone/UI/UpgradeMenu/UpgradeMenuWidget.h
@@ -0,0 +1,17 @@
+// Created by Spring2022_Capstone team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UpgradeMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPRING2022_CAPSTONE_API UUpgradeMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
