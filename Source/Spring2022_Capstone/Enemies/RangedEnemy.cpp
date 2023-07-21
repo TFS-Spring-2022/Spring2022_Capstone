d4a2db58c282f5ddd549a14514e68313e2c81696
Taisen Colcher <TaisenColcher@Gmail.com>
2023-07-20 22:56:54 -0400

    Add ToDo Comments
    Basic/starter plan for my enemy attacking system.

--- Source/Spring2022_Capstone/Enemies/RangedEnemy.cpp
+++ Source/Spring2022_Capstone/Enemies/RangedEnemy.cpp
@@ -13,11 +13,16 @@ ARangedEnemy::ARangedEnemy()
 void ARangedEnemy::Attack()
 {
 	// Test Attack, Just here to show Damage Indicator functionality.
-	APlayerCharacter* Player = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
-	Player->DamageActor(this, 10.0f);
+	// APlayerCharacter* Player = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
+	// Player->DamageActor(this, 10.0f);
+
+	// ToDo: GetPlayerLocation
+	// ToDo: Raycast towards play (Need FHitResults to play particle effects.
+
+	// ToDo: Random chance to move Raycast and miss player
 }
 
 void ARangedEnemy::Reload()
 {
-
+	// ToDo: Stop player from moving/attacking while they reload
 }
