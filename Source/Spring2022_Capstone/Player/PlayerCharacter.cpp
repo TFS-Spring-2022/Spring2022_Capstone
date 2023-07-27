002b6bd8ce2adfdb678bc0cd96f8a2da50959396
kaitlynli64 <kaitlyn.linda.li@hotmail.com>
2023-07-27 17:38:47 -0400

    Implemented Crystal and Mud Hazards

--- Source/Spring2022_Capstone/Player/PlayerCharacter.cpp
+++ Source/Spring2022_Capstone/Player/PlayerCharacter.cpp
@@ -170,6 +170,10 @@ void APlayerCharacter::Dash(const FInputActionValue &Value)
 	PreviousDashDirection = Value.GetMagnitude();
 }
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 void APlayerCharacter::DashDirectionalLaunch()
 {
 	const float PreDashSpeed = GetVelocity().Length();
