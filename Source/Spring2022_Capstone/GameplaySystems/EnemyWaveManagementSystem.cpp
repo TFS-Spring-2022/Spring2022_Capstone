415c9d5b32615ff5c8ad5d9d54ed0ceb8f1625ca
Taisen Colcher <TaisenColcher@Gmail.com>
2023-07-28 00:14:35 -0400

    Make EnemyWaveManagementSystem Blueprintable and BlueprintType.
    Changed the EnemyWaveManagementSystem to be blueprintable for creation in the game modes.
    
    Added a public float property and a public void Debug_PublicCall() to ensure the game mode was creating the proper instances of managers. (To be removed).

--- Source/Spring2022_Capstone/GameplaySystems/EnemyWaveManagementSystem.cpp
+++ Source/Spring2022_Capstone/GameplaySystems/EnemyWaveManagementSystem.cpp
@@ -3,3 +3,7 @@
 
 #include "EnemyWaveManagementSystem.h"
 
+void UEnemyWaveManagementSystem::DEBUG_PublicCall()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Instance : %f"), DEBUG_FloatValue);
+}
--- Source/Spring2022_Capstone/GameplaySystems/EnemyWaveManagementSystem.h
+++ Source/Spring2022_Capstone/GameplaySystems/EnemyWaveManagementSystem.h
@@ -9,9 +9,17 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable, BlueprintType)
 class SPRING2022_CAPSTONE_API UEnemyWaveManagementSystem : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(EditAnywhere, Category = "DEBUG")
+	float DEBUG_FloatValue;
+	
+	// Using to test, just prints to UE_Log the value of DEBUG_FloatValue.
+	void DEBUG_PublicCall();
 	
 };
