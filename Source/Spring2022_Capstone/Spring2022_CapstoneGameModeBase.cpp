82e7e3c82cc85f657e082e4cf7116e5f5406f8fe
Taisen Colcher <TaisenColcher@Gmail.com>
2023-07-28 00:13:01 -0400

    Add UEnemyWaveManagementSystem Reference in Spring2022_CapstoneGameModeBase
    Created a TSubclassOf<UEnemyWave>... to create an instance of a given EnemyWaveManager blueprint.
    
    Created said instance in BeginPlay() and set a call to unimplemented test function.

--- Source/Spring2022_Capstone/Spring2022_CapstoneGameModeBase.cpp
+++ Source/Spring2022_Capstone/Spring2022_CapstoneGameModeBase.cpp
@@ -3,3 +3,12 @@
 
 #include "Spring2022_CapstoneGameModeBase.h"
 
+void ASpring2022_CapstoneGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Create an instance of EnemyWaveManager BP assigned in the details panel. 
+	EnemyWaveManagerInstance = NewObject<UEnemyWaveManagementSystem>(this, LevelEnemyWaveManagerBP);
+	EnemyWaveManagerInstance->DEBUG_PublicCall(); // Quick test to ensure proper creation.
+
+}
--- Source/Spring2022_Capstone/Spring2022_CapstoneGameModeBase.h
+++ Source/Spring2022_Capstone/Spring2022_CapstoneGameModeBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "GameplaySystems/EnemyWaveManagementSystem.h"
 #include "Spring2022_CapstoneGameModeBase.generated.h"
 
 /**
@@ -13,5 +14,15 @@ UCLASS()
 class SPRING2022_CAPSTONE_API ASpring2022_CapstoneGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+	virtual void BeginPlay() override;
+
+	// The Enemy Wave Manager BP for the level.
+	UPROPERTY(EditDefaultsOnly, Category = "Wave Management")
+	TSubclassOf<UEnemyWaveManagementSystem> LevelEnemyWaveManagerBP;
+
+	// An instance of the LevelEnemyWaveManagerBP.
+	UPROPERTY()
+	UEnemyWaveManagementSystem* EnemyWaveManagerInstance;
 	
 };
