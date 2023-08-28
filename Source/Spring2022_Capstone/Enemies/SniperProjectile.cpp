699c847af9a1b8eaa3f683728160fc2b2e7d0bcc
Victor Ulloa <alejandroekas@gmail.com>
2023-08-28 15:06:36 -0400

    sniper projectile class created

---
+++ Source/Spring2022_Capstone/Enemies/SniperProjectile.cpp
@@ -0,0 +1,25 @@
+// Created by Spring2022_Capstone team
+
+#include "SniperProjectile.h"
+#include "Spring2022_Capstone/GameplaySystems/DamageableActor.h"
+#include "Spring2022_Capstone/Player/PlayerCharacter.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void ASniperProjectile::BeginPlay()
+{
+    Super::BeginPlay();
+
+    ProjectileMovementComponent->MaxSpeed = 300.f;
+	ProjectileMovementComponent->InitialSpeed = 300.f;
+	ProjectileMovementComponent->ProjectileGravityScale = .0f;
+	ProjectileMovementComponent->bRotationFollowsVelocity = true;
+
+    ProjectileMesh->OnComponentHit.AddDynamic(this, &ASniperProjectile::OnHit);
+}
+
+void ASniperProjectile::OnHit(UPrimitiveComponent *HitComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, FVector NormalizedImpulse, const FHitResult &Hit)
+{
+    // TODO: Explosive round
+    Destroy();
+}
