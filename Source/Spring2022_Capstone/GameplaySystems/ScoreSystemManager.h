ecfc9283dc2b27829e7ca097be23c602e465734c
Taisen Colcher <TaisenColcher@Gmail.com>
2023-08-31 04:11:40 -0400

    Create Score System Manager Class
    Created a UObject derived ScoreSystemManager class to be created in the GameInstance and used to handle score and accolades through a run.

---
+++ Source/Spring2022_Capstone/GameplaySystems/ScoreSystemManager.h
@@ -0,0 +1,17 @@
+// Created by Spring2022_Capstone team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ScoreSystemManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPRING2022_CAPSTONE_API UScoreSystemManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
