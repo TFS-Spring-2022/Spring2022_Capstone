9fdb052fe623b55d7d2f65a50b9ac9bfc837a608
Taisen Colcher <TaisenColcher@Gmail.com>
2023-08-31 02:55:26 -0400

    Implement Disable/Enable Functions In Sniper Enemy
    - Created Disable/EnableSniperEnemy().
    - Added  a bool bCanAttack which is toggled through above functions and stops/lets the enemy shoot.
    -

--- Source/Spring2022_Capstone/Enemies/SniperEnemy.cpp
+++ Source/Spring2022_Capstone/Enemies/SniperEnemy.cpp
@@ -5,6 +5,30 @@
 
 void ASniperEnemy::Attack()
 {
-    //TODO: Implementation
-    UE_LOG(LogTemp, Display, TEXT("ATTACK"));
+    if(bCanAttack)
+    {
+        //TODO: Implementation
+        UE_LOG(LogTemp, Display, TEXT("ATTACK"));
+    }
+}
+
+void ASniperEnemy::BeginPlay()
+{
+    Super::BeginPlay();
+
+    EnableSniperEnemy();
+}
+
+void ASniperEnemy::DisableSniperEnemy()
+{
+    GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, GetName() + "Sniper Enemy Disabled");
+    bCanAttack = false;
+    // ToDo: Disable laser effect (when implemented).
+}
+
+void ASniperEnemy::EnableSniperEnemy()
+{
+    GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, GetName() + "Sniper Enemy Enabled");
+    bCanAttack = true;
+    // ToDo: Enable laser effect (when implemented).
 }
--- Source/Spring2022_Capstone/Enemies/SniperEnemy.h
+++ Source/Spring2022_Capstone/Enemies/SniperEnemy.h
@@ -13,7 +13,18 @@ UCLASS()
 class SPRING2022_CAPSTONE_API ASniperEnemy : public ABaseEnemy
 {
 	GENERATED_BODY()
-	
+
 protected:
 	void Attack() override;
+
+	bool bCanAttack = true;
+
+	virtual void BeginPlay() override;
+
+public:
+
+	void DisableSniperEnemy();
+
+	void EnableSniperEnemy();
+	
 };
