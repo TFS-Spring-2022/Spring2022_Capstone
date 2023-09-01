89baaec98a48efdb8b8d405adf3e900acce91e0d
Taisen Colcher <TaisenColcher@Gmail.com>
2023-08-31 20:35:12 -0400

    Create SkyPirateGameInstance Class
    - Created a GameInstance Derived class SKyPirateGameInstance.
    - Set it as the Game Instance Class inside Project Settings

---
+++ Source/Spring2022_Capstone/SkyPirateGameInstance.h
@@ -0,0 +1,17 @@
+// Created by Spring2022_Capstone team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SkyPirateGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPRING2022_CAPSTONE_API USkyPirateGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
