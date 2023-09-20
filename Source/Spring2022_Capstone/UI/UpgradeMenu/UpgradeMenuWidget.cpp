fd78c6c317142305561f0662c097366d8640804d
Gabriel Massé <Gabriel_Masse@outlook.com>
2023-09-19 23:17:44 -0400

    Taisen
    eat poo

--- Source/Spring2022_Capstone/UI/UpgradeMenu/UpgradeMenuWidget.cpp
+++ Source/Spring2022_Capstone/UI/UpgradeMenu/UpgradeMenuWidget.cpp
@@ -12,14 +12,23 @@ void UUpgradeMenuWidget::SetUpgradeTextBox(int UpgradeChoiceNumber, FText Upgrad
 	case 1:
 		Upgrade1Text->SetText(UpgradeText);
 		Upgrade1Button->WidgetStyle.Normal.SetResourceObject(Image);
+		Upgrade1Button->WidgetStyle.Hovered.SetResourceObject(Image);
+		Upgrade1Button->WidgetStyle.Pressed.SetResourceObject(Image);
+		Upgrade1Button->WidgetStyle.Disabled.SetResourceObject(Image);
 		break;
 	case 2:
 		Upgrade2Text->SetText(UpgradeText);
 		Upgrade2Button->WidgetStyle.Normal.SetResourceObject(Image);
+		Upgrade2Button->WidgetStyle.Hovered.SetResourceObject(Image);
+		Upgrade2Button->WidgetStyle.Pressed.SetResourceObject(Image);
+		Upgrade2Button->WidgetStyle.Disabled.SetResourceObject(Image);
 		break;
 	case 3:
 		Upgrade3Text->SetText(UpgradeText);
 		Upgrade3Button->WidgetStyle.Normal.SetResourceObject(Image);
+		Upgrade3Button->WidgetStyle.Hovered.SetResourceObject(Image);
+		Upgrade3Button->WidgetStyle.Pressed.SetResourceObject(Image);
+		Upgrade3Button->WidgetStyle.Disabled.SetResourceObject(Image);
 		break;
 	default:
 		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, "Error - Upgrade box to update doesn't exist");
